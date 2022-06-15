6c1bc482579c982f112a686920941143617056ce
Daniel Medina <daniel@medina.com>
2022-06-15 18:10:46 -0500

    :mag: Added twitter meta tags and google meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,17 +14,41 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <title>VOLTEC Robotics 6647</title>
         <meta
           name="description"
           content="⚡️MTY, México. Equipo Representativo de Robótica de @prepatec.egl"
         />
-        <meta name="og:image" content="/voltec.png" />
-        <meta name="og:title" content="VOLTEC Robotics 6647" />
+
+        <meta itemProp="name" content="VOLTEC Robotics 6647" />
         <meta
-          name="og:description"
+          itemProp="description"
           content="⚡️MTY, México. Equipo Representativo de Robótica de @prepatec.egl"
         />
-        <meta name="og:url" content="https://voltec.medina.dev" />
+        <meta itemProp="image" content="https://voltec.medina.dev/voltec.png" />
+
+        <meta property="og:url" content="https://voltec.medina.dev" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="VOLTEC Robotics 6647" />
+        <meta
+          property="og:description"
+          content="⚡️MTY, México. Equipo Representativo de Robótica de @prepatec.egl"
+        />
+        <meta
+          property="og:image"
+          content="https://voltec.medina.dev/voltec.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="VOLTEC Robotics 6647" />
+        <meta
+          name="twitter:description"
+          content="⚡️MTY, México. Equipo Representativo de Robótica de @prepatec.egl"
+        />
+        <meta
+          name="twitter:image"
+          content="https://voltec.medina.dev/voltec.png"
+        />
       </Head>
       <body>
         <Main />
