80cbfb7c78006a3fc2fca8cc2ec9a8671e6894ce
Daniel Medina <daniel@medina.com>
2022-06-22 21:51:48 -0500

    :bug: Fixed visual issues in mobile admin page :3

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -19,14 +19,14 @@ const index = () => {
           />
         </div>
       </div>
-      <div className="max-w-6xl mx-auto py-14">
-        <div className="headings pt-4 lg:pt-0 pb-8">
+      <div className="max-w-6xl mx-auto py-14 bg-background-blue">
+        <div className="headings pt-4 lg:pt-0 pb-8 px-5">
           <h1 className="title text-white pb-5">
             Panel de <span className="text-pink-400">Administración</span> 🔧
           </h1>
           <hr />
         </div>
-        <div className="tools grid grid-cols-2 gap-4">
+        <div className="tools grid grid-cols-1 md:grid-cols-2 gap-4 px-5">
           <Link href="/admin/blog">
             <div className="tool topography p-5 border-2 border-gray-200 rounded-xl cursor-pointer flex flex-col gap-4">
               <div className="relative h-80">
@@ -38,7 +38,7 @@ const index = () => {
                   objectFit="cover"
                 />
               </div>
-              <a className="text-4xl text-white font-black hover:underline">
+              <a className="text-2xl md:text-4xl text-white font-black hover:underline">
                 Administrar Blog 💬
               </a>
             </div>
@@ -54,7 +54,7 @@ const index = () => {
                   objectFit="cover"
                 />
               </div>
-              <a className="text-4xl text-white font-black hover:underline">
+              <a className="text-2xl md:text-4xl text-white font-black hover:underline">
               Administrar Usuarios 👨‍💻
               </a>
             </div>
