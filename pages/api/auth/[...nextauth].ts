063ddf9b90d08dcbc9d0efc447a740c2c1bcc7e7
Daniel Medina <daniel@medina.com>
2022-07-12 13:45:45 -0500

    :lock: Made signin only available for admins with the correct email domain.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,20 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
 
+const getAdmins = async () => {
+  const client = await clientPromise;
+  const db = client.db("Users");
+  const collection = db.collection("Admins");
+  const adminsArray = await collection
+    .find({})
+    .project({ email: 1, _id: 0 })
+    .toArray();
+  const admins = adminsArray.map((admin) => admin.email);
+
+  console.log(admins);
+  return admins;
+};
+
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
@@ -12,6 +26,19 @@ export const authOptions: NextAuthOptions = {
   ],
   pages: {
     signIn: "/signin",
+  },
+  callbacks: {
+    signIn: async ({ user, account, profile }) => {
+      const admins = await getAdmins();
+      if (
+        admins.indexOf(profile.email) > -1 ||
+        profile.email?.endsWith("@tec.mx")
+      ) {
+        return Promise.resolve(true);
+      } else {
+        return Promise.resolve(false);
+      }
+    },
   },
   secret: process.env.SECRET,
   adapter: MongoDBAdapter(clientPromise),
